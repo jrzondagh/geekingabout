db0efaeb6170d8cfe4a56cdd31392071dcb1ef0d
root <root@raspberrypi.(none)>
2013-06-05 23:38:25 +0100

    added lapse.py

--- lapse.py
+++ lapse.py
@@ -26,11 +26,18 @@ Passed = 0
 # The pulse width is the total time to execute one pulse - i.e. On and Off
 pulseWidth = 0.01
 
+motorPin = 0
+shutterPin = 0
+focusPin = 0
+counter = 0
 
 if(bool(sys.argv[1]) and bool(sys.argv[2])):
 
 	try:
 		motorPin = int(sys.argv[1])
+		focusPin = 0
+		shutterPin = 23
+
 		runTime = float(sys.argv[2])
 		powerPercentage = float(sys.argv[3]) / 100
 		Passed = 1
@@ -42,25 +49,33 @@ if Passed:
 	# Set all pins as output
 	print "Setup Motor Pin"
 	GPIO.setup(motorPin,GPIO.OUT)
+	GPIO.setup(shutterPin, GPIO.OUT)
+
 	GPIO.output(motorPin, False)
-	counter = int(runTime / pulseWidth)
+	GPIO.output(shutterPin, False)
 
-	print "Start Motor" 
+	counter = 10
 
-	print "Power: " + str(powerPercentage)
-	onTime = pulseWidth * powerPercentage
-	offTime = pulseWidth - onTime
+	intervalSec = 1
+	timeMotor = 0.05
+	timeShutterRelease = 0.001
+	timeExpose = float(1/13)
+	timeSleep = intervalSec - timeMotor - timeShutterRelease - timeExpose 
 
 	while counter > 0:
 		GPIO.output(motorPin, True)
-		time.sleep(onTime)
+		time.sleep(timeMotor)
 		GPIO.output(motorPin, False)
-		time.sleep(offTime)
+		time.sleep(timeSleep)
+		GPIO.output(shutterPin, True)
+		time.sleep(timeShutterRelease)
+		GPIO.output(shutterPin, False)
+		time.sleep(timeExpose)
 		counter = counter - 1
 
 	print "Stop Motor"
 	GPIO.output(motorPin, False)
-
+	GPIO.output(shutterPin, False)
 
 else:
         print "Usage: motor.py GPIO_Pin_Number Seconds_To_Turn Power_Percentage"
